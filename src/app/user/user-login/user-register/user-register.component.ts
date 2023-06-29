622afed26bf65b99f023beac6665bd333a40e89e
dknic <dknic101993@gmail.com>
2023-06-29 23:56:26 +0530

    save data to local storage

--- src/app/user/user-login/user-register/user-register.component.ts
+++ src/app/user/user-login/user-register/user-register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup,Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup,Validators } from '@angular/forms';
+import { User } from 'src/app/model/user';
 
 @Component({
   selector: 'app-user-register',
@@ -7,24 +8,46 @@ import { FormControl, FormGroup,Validators } from '@angular/forms';
   styleUrls: ['./user-register.component.css']
 })
 export class UserRegisterComponent implements OnInit {
- registrationform:FormGroup
+ registrationform:FormGroup;
+ usersubmitted:boolean;
 
-  constructor() { }
+ user:User
+
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
-    this.registrationform= new FormGroup({
+   /* this.registrationform= new FormGroup({
      userName:new FormControl(null,Validators.required),
      email:new FormControl(null,[Validators.required,Validators.email]),
      password:new FormControl(null,[Validators.required,Validators.minLength(8)]),
      confirmpassword:new FormControl(null,[Validators.required,Validators.minLength(8)]),
       mobile:new FormControl(null,Validators.required)
-    } );
+    } );*/
+    this.createUserRegistration();
   }
 
-  /*passwordMatchingValidator(fg:FormGroup):Validators{
+  passwordMatchingValidator(fg:FormGroup):Validators{
     return fg.get('password')?.value=== fg.get('confirmpassword')?.value ?true:{notmatched:true}
-  }*/
+  }
 
+  createUserRegistration(){
+    this.registrationform=this.fb.group({
+      userName:new FormControl(null,Validators.required),
+      email:new FormControl(null,[Validators.required,Validators.email]),
+      password:new FormControl(null,[Validators.required,Validators.minLength(8)]),
+      confirmpassword:new FormControl(null,[Validators.required,Validators.minLength(8)]),
+       mobile:new FormControl(null,Validators.required)
+    },{Validators:this.passwordMatchingValidator})
+  }
+
+  /*userData(User){
+    return this.user={
+      userName:this.userName.value,
+      email:this.email.value,
+      password:this.password.value,
+      mobile:this.mobile.value
+    }
+  }*/
   get userName(){
     return this.registrationform.get('userName') as FormControl
   }
@@ -32,9 +55,37 @@ export class UserRegisterComponent implements OnInit {
   get email(){
     return this.registrationform.get('email') as FormControl
   }
+  get mobile(){
+    return this.registrationform.get('mobile') as FormControl
+  }
+  get password(){
+    return this.registrationform.get('password') as FormControl
+  }
 
   onSubmit(){
+    this.usersubmitted=true
+    if(this.registrationform.valid){
     console.log(this.registrationform)
+    this.user=Object.assign(this.user,this.registrationform.value);
+    localStorage.setItem('Users',JSON.stringify(this.user))// conver json into string
+    this.registrationform.reset();
+    this.usersubmitted=false;
   }
+  }
+
+  
+
+ /* adduser(user:User){
+
+    let users=[];
+
+    if (localStorage.getItem('Users')){
+      users=JSON.parse(localStorage.getItem('Users'));// convert string into json
+      users=[user,...users];
+    } else {
+      users=[user];
+    }
+    localStorage.setItem('Users',JSON.stringify(this.users))// conver json into string
+  }*/
 
 }
